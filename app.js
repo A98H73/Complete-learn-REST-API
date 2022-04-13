0206464299c00fc3c65646ce32c39382a2b144e2
A98H73 <singh002abhijeet@gmail.com>
2022-04-13 22:11:26 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const StudentRoute = require('./api/routes/student');
-const FacultyRouter = require('./api/routes/faculty')
+const FacultyRouter = require('./api/routes/faculty');
+const req = require('express/lib/request');
 
 
 app.use('/student', StudentRoute)
 
 app.use('/faculty', FacultyRouter)
 
+
+
+// ERROR HANDLING FOR BACKEND 
+
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'humara app is running bhai'
+    res.status(404).json({
+        error: "BAD REQUEST"
     })
 })
 
